796c9a31f74032ce39e58f1c5256cbddd72f9f24
preetham-11 <preetursp@gmail.com>
2025-08-15 01:16:16 +0530

    Data Transformation

--- main.py
+++ main.py
@@ -3,25 +3,35 @@ from networksecurity.logging.logger import logging
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
 
 if __name__ == '__main__':
     try:
         trainingpipelineconfig = TrainingPipelineConfig()
+
         dataingestionconfig = DataIngestionConfig(trainingpipelineconfig)
         data_ingestion = DataIngestion(dataingestionconfig)
         logging.info("Initiate the data ingestion")
         dataingestionartifact = data_ingestion.initiate_data_ingestion()
         logging.info("Data Ingestion completed")
         print(dataingestionartifact)
+
         data_validation_config = DataValidationConfig(trainingpipelineconfig)
         data_validation = DataValidation(dataingestionartifact, data_validation_config)
         logging.info("Initiate the data validation")
         data_validation_artifact = data_validation.initiate_data_validation()
         logging.info("Data Validation completed")
         print(data_validation_artifact)
+
+        data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
+        data_transformation = DataTransformation(data_validation_artifact, data_transformation_config)
+        logging.info("Initiate the data transformation")
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        logging.info("Data Transformation completed")
+        print(data_transformation_artifact)
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
